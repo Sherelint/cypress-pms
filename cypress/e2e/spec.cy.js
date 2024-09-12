665bc4d8de00dee3a3f9e3a8387f1ad88c471ac0
Sherelin Thomas <sherelinthomasf@gmail.com>
2024-09-11 20:14:13 -0500

    adding a better git ignore

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:5173/')
+  cy.get('input[id="email"]').type("fdegracia@msupply.com.pa")
+  cy.get('input[id="password"]').type("12345")
+  cy.get('button').contains("Iniciar Sesión").click()
+
+  cy.get('button').contains('Create Report').click()
+  cy.get('h5').contains('Create Report').should('be.visible')
   })
 })
