4e05d5f991274e27980a5c968232a052ed9b1281
Sherelin Thomas <sherelinthomasf@gmail.com>
2024-09-30 10:53:31 -0500

    adding operation details

--- cypress/e2e/operationdetails.cy.js
+++ cypress/e2e/operationdetails.cy.js
@@ -5,37 +5,18 @@ describe("template spec", () => {
     cy.get('input[id="email"]').type("fdegracia@msupply.com.pa");
     cy.get('input[id="password"]').type("12345");
     cy.get("button").contains("Iniciar Sesión").click();
+    cy.get("td").contains("1000").click();
 
-    cy.get("button").contains("Create Report").click();
     cy.get('svg[data-testid="MenuIcon"]').click();
     cy.get("div").contains("Operation Details").click();
-
-    cy.get("button").contains('svg[data-testid="CalendarIcon"]').click()
     
+    cy.get('button[aria-label="Choose date"]').each(($el, index, $list) => {
+      cy.wrap($el).click();
+      cy.get('button[aria-current="date"]').click({multiple: true});
+      cy.get("button").contains("OK").click({multiple: true});
+    });
     
-    cy.get(".MuiPickersDay-today").click();
-    //() => {
-
-    const randomnumber = Math.floor(Math.random() * 30) + 1;
-    const randomstring = randomnumber.toString();
-
-    const randomHour = Math.floor(Math.random() * 12) + 1;
-    const HourString = randomHour.toString().padStart(2, "0");
-
-    const randomIndex = Math.floor(Math.random() * 12);
-    const randomMinutes = randomIndex * 5;
-    const MinutesString = randomMinutes.toString().padStart(2, "0");
-
-    const isPM = Math.random() < 0.5;
-    const period = isPM ? "PM" : "AM";
-
-    cy.get("button").contains(randomstring).click();
-    cy.get("li").contains(HourString).click();
-    cy.get("li").contains(MinutesString).click();
-    cy.get("li").contains(period).click();
-    cy.get("button").contains("OK").click();
-    //});
-
     cy.get("button").contains("Continue").click();
+
   });
 });
